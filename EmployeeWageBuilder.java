863badc778bdb0a956053b4210a056d76a22afaa
Jagriti <jagriti.agni@gmail.com>
2020-09-22 19:58:15 +0530

    [Jagriti]ADDED: Total wage of a month using for loop

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,38 +3,36 @@ public class EmployeeWageBuilder{
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final	int EMP_RATE_PER_HOUR = 20;
-
+	public static final int NUM_OF_WORKING_DAYS = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-		// variables
-		int empHours = 0;
-		int empWage = 0;
-
-		int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
+		// variables 
+		int empHours = 0, empWage = 0, totalEmpWage = 0;
 		
-		// Computation UC4
-		switch (empCheck) {
-			case IS_FULL_TIME:
-				empHours = 8;
-				break;
+		// computation for UC5
+		for (int day = 0; day < NUM_OF_WORKING_DAYS ; day++) {
 
-			case IS_PART_TIME:
-				empHours = 4;
-				break;
+			int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;      // randomly a number selected 
+		
+			switch (empCheck) {									// calculates the number of working hours
+				case IS_FULL_TIME:
+					empHours = 8;
+					break;
 
-			default: 
-				empHours = 0;
+				case IS_PART_TIME:
+					empHours = 4;
+					break;
+
+				default: 
+					empHours = 0;
+			}
+			empWage = empHours * EMP_RATE_PER_HOUR;					// wage of emp based of working hours 
+			totalEmpWage += empWage;								// wages of emp added
+			System.out.println("Emp wage: " + empWage);
 		}
-		empWage = empHours * EMP_RATE_PER_HOUR;
-		System.out.println("Emp wage: " + empWage);
+		System.out.println("Total Emp wage:" + totalEmpWage);
 
-		// Computation UC1 
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is absent");
-		}
 	}
 }
