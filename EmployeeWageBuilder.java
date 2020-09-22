4e36cda905329c869e5dae35f58a7e03e9cfccc1
Jagriti <jagriti.agni@gmail.com>
2020-09-22 20:43:40 +0530

    [Jagriti]ADDED: total month wage with max working hours

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,16 +4,18 @@ public class EmployeeWageBuilder{
 	public static final int IS_FULL_TIME = 2;
 	public static final	int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		// variables 
-		int empHours = 0, empWage = 0, totalEmpWage = 0;
+		int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
 		
-		// computation for UC5
-		for (int day = 0; day < NUM_OF_WORKING_DAYS ; day++) {
-
+		// computation for UC6
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			int empHours = 0;
+			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;      // randomly a number selected 
 		
 			switch (empCheck) {									// calculates the number of working hours
@@ -28,7 +30,8 @@ public class EmployeeWageBuilder{
 				default: 
 					empHours = 0;
 			}
-			empWage = empHours * EMP_RATE_PER_HOUR;					// wage of emp based of working hours 
+			totalEmpHrs += empHours;
+			int empWage = empHours * EMP_RATE_PER_HOUR;					// wage of emp based of working hours 
 			totalEmpWage += empWage;								// wages of emp added
 			System.out.println("Emp wage: " + empWage);
 		}
