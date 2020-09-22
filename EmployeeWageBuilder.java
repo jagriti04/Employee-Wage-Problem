4755b4b31dc4f7996778fee40d1e454624bb4d1f
Jagriti <jagriti.agni@gmail.com>
2020-09-22 18:27:39 +0530

    [Jagriti]ADDED: Emp daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,14 +1,28 @@
 public class EmployeeWageBuilder{
 	
 	public static void main(String[] args) {
-		System.out.print("Welcome to Employee Wage Computation Program\n");
+		System.out.println("Welcome to Employee Wage Computation Program");
 
 		// constant
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
 
-		// variable
+		// variables
+		int empHours = 0;
+		int empWage = 0;
+
 		double empCheck = Math.floor(Math.random() * 10 ) % 2;
 		
+
+		// Computation UC2
+		if (empCheck == IS_FULL_TIME) {
+			empHours = 8;
+		} else {
+			empHours = 0;
+		}
+		empWage = empHours * EMP_RATE_PER_HOUR;
+		System.out.println("Emp wage: " + empWage);
+
 		// Computation UC1 
 		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is present");
