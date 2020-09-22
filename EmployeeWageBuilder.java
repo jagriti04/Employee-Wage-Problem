94d92fb87843db7761b8fed471292bd80465540f
Jagriti <jagriti.agni@gmail.com>
2020-09-22 19:47:08 +0530

    [Jagriti]ADDED: Switch case to find emp daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,25 +1,31 @@
 public class EmployeeWageBuilder{
-	
+	// constant
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final	int EMP_RATE_PER_HOUR = 20;
+
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-
-		// constant
-		int IS_PART_TIME = 1;
-		int IS_FULL_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
 
 		// variables
 		int empHours = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10 ) % 3;
+		int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
 		
+		// Computation UC4
+		switch (empCheck) {
+			case IS_FULL_TIME:
+				empHours = 8;
+				break;
 
-		// Computation UC3
-		if (empCheck == IS_FULL_TIME) {
-			empHours = 8;
-		} else if (empCheck == IS_PART_TIME) {
-			empHours = 4;
+			case IS_PART_TIME:
+				empHours = 4;
+				break;
+
+			default: 
+				empHours = 0;
 		}
 		empWage = empHours * EMP_RATE_PER_HOUR;
 		System.out.println("Emp wage: " + empWage);
