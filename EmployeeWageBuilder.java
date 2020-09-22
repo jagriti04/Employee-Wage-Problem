df29d1c9a4d93624a2a7324939ecccd53d6d0bc3
Jagriti <jagriti.agni@gmail.com>
2020-09-22 15:04:30 +0530

    [Jagriti]Added:Emp is present or not

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,19 @@
 public class EmployeeWageBuilder{
+	
 	public static void main(String[] args) {
-		System.out.print("Welcome to Employee Wage Computation Program");
+		System.out.print("Welcome to Employee Wage Computation Program\n");
+
+		// constant
+		int IS_FULL_TIME = 1;
+
+		// variable
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		
+		// Computation UC1 
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
 	}
 }
