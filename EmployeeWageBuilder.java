b3fe78f5e5dd57744557e16eadc20ceabb000e1c
Jagriti <jagriti.agni@gmail.com>
2020-09-26 00:25:55 +0530

    [Jagriti]REFACTORED- emp wage calculated using class methods

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,18 +2,13 @@ public class EmployeeWageBuilder{
 	// constant
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final	int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+	public static int calcEmplWageForCompany (String company, int empRate, 
+												int numOfDays, int maxHours) {
 		// variables 
 		int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
 		
-		// computation for UC6
-		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		while (totalEmpHrs <= maxHours && totalWorkingDays < numOfDays) {
 			int empHours = 0;
 			totalWorkingDays++;
 			int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;      // randomly a number selected 
@@ -31,11 +26,18 @@ public class EmployeeWageBuilder{
 					empHours = 0;
 			}
 			totalEmpHrs += empHours;
-			int empWage = empHours * EMP_RATE_PER_HOUR;					// wage of emp based of working hours 
+			int empWage = empHours * empRate;					// wage of emp based of working hours 
 			totalEmpWage += empWage;								// wages of emp added
 			System.out.println("Emp wage: " + empWage);
 		}
-		System.out.println("Total Emp wage:" + totalEmpWage);
+		System.out.println("Total Emp wage for company: "+ company+" " + totalEmpWage);
+		return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+		calcEmplWageForCompany("DMart", 20, 2, 10);
+		
 
 	}
 }
